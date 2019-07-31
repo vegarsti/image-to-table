64a5e618def321ee956829fe28f3fe878bd25558
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-07-31 16:38:20 +0200

    Initial commit

---
+++ base64_encode.py
@@ -0,0 +1,32 @@
+import base64
+import io
+path = "example.png"
+with open(path, "rb") as img_file:
+    my_string = base64.b64encode(img_file.read())
+print(my_string)
+
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+
+detect_text(path)
