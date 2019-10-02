a18b57ab037e95313801301cc87e15b5a4ac040c
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-10-02 20:12:16 +0200

    Actually add black

--- base64_encode.py
+++ base64_encode.py
@@ -1,5 +1,6 @@
 import base64
 import io
+
 path = "example.png"
 with open(path, "rb") as img_file:
     my_string = base64.b64encode(img_file.read())
@@ -9,24 +10,22 @@ print(my_string)
 def detect_text(path):
     """Detects text in the file."""
     from google.cloud import vision
+
     client = vision.ImageAnnotatorClient()
 
-    with io.open(path, 'rb') as image_file:
+    with io.open(path, "rb") as image_file:
         content = image_file.read()
 
     image = vision.types.Image(content=content)
 
     response = client.text_detection(image=image)
     texts = response.text_annotations
-    print('Texts:')
+    print("Texts:")
 
     for text in texts:
-        print('\n"{}"'.format(text.description))
+        vertices = ["({},{})".format(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices]
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
-
-        print('bounds: {}'.format(','.join(vertices)))
+        print("bounds: {}".format(",".join(vertices)))
 
 
 detect_text(path)
