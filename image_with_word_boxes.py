2bc948073d34c3bee83da7653db7e18420a78b17
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-10-16 20:25:15 +0200

    Working extractor using geometry

--- image_with_word_boxes.py
+++ image_with_word_boxes.py
@@ -2,11 +2,13 @@ import io
 from dataclasses import dataclass
 
 import cv2
+import matplotlib.pyplot as plt
 from google.cloud import vision
+from shapely import geometry
 
 
 @dataclass
-class Corner:
+class Point:
     x: int
     y: int
 
@@ -16,10 +18,14 @@ class Corner:
 
 @dataclass
 class Box:
-    lower_left_corner: Corner
-    lower_right_corner: Corner
-    upper_right_corner: Corner
-    upper_left_corner: Corner
+    lower_left_corner: Point
+    lower_right_corner: Point
+    upper_right_corner: Point
+    upper_left_corner: Point
+    text: str = None
+
+    def is_inside_box(self, outer_box):  # x \in X
+        return outer_box.as_polygon().contains(self.as_polygon()) or outer_box.as_polygon().overlaps(self.as_polygon())
 
     def point_inside(self, x, y):
         return (
@@ -32,6 +38,28 @@ class Box:
 
     def y_inside(self, y):
         return self.lower_left_corner.y <= y <= self.upper_right_corner.y
+
+    def min_x(self):
+        return min(
+            corner.x
+            for corner in (
+                self.lower_left_corner,
+                self.lower_right_corner,
+                self.upper_right_corner,
+                self.lower_right_corner,
+            )
+        )
+
+    def min_y(self):
+        return min(
+            corner.y
+            for corner in (
+                self.lower_left_corner,
+                self.lower_right_corner,
+                self.upper_right_corner,
+                self.lower_right_corner,
+            )
+        )
 
     def max_x(self):
         return max(
@@ -55,6 +83,18 @@ class Box:
             )
         )
 
+    def as_polygon(self):
+        polygon = geometry.Polygon(
+            (p.x, p.y)
+            for p in (self.lower_left_corner, self.lower_right_corner, self.upper_right_corner, self.upper_left_corner)
+        )
+        assert polygon.is_valid
+        return polygon
+
+    def plot(self, color):
+        x, y = self.as_polygon().exterior.xy
+        plt.plot(x, y, color=color)
+
 
 def detect_text(image_path):
     """Detects text in the file."""
@@ -66,22 +106,31 @@ def detect_text(image_path):
     texts = response.text_annotations
 
     boxes = []
-    for text in texts:
-        corners = [Corner(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices]
-        box = Box(*corners)
+    for i, text in enumerate(texts):
+        lower_left_corner, lower_right_corner, upper_right_corner, upper_left_corner = [
+            Point(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices
+        ]
+        box = Box(
+            text=text.description,
+            lower_left_corner=lower_left_corner,
+            lower_right_corner=lower_right_corner,
+            upper_left_corner=upper_left_corner,
+            upper_right_corner=upper_right_corner,
+        )
         boxes.append(box)
     return boxes
 
 
-def show_image_with_word_boxes(filename, boxes):
+def show_image_with_word_boxes(filename, boxes=None):
     image = cv2.imread(filename)
-    height, width, channels = image.shape
     color = (0, 0, 0)
     thickness = 2
     # skip first, large box with everything
+    if boxes is None:
+        boxes = []
     for box in boxes:
         image = cv2.rectangle(
-            image, box.upper_left_corner.as_tuple(), box.lower_right_corner.as_tuple(), color, thickness
+            image, box.lower_left_corner.as_tuple(), box.upper_right_corner.as_tuple(), color, thickness
         )
     cv2.imshow("table", image)
     while True:
@@ -99,27 +148,7 @@ def boxes_in_y(boxes, y):
     return [box for box in boxes if box.y_inside(y)]
 
 
-filename = "example.png"
-
-image = cv2.imread(filename)
-height, width, _ = image.shape
-
-boxes = detect_text(filename)[1:]
-
-max_x = max(box.max_x() for box in boxes)
-x_counts = [len(boxes_in_x(boxes, x)) for x in range(max_x)]
-
-max_y = max(box.max_y() for box in boxes)
-boxes_in_ys = [boxes_in_y(boxes, y) for y in range(max_y)]
-y_counts = [len(l) for l in boxes_in_ys]
-
-print(f"height {height}, width {width}, max_x {max_x}, max_y {max_y}")
-
-max_x = width
-max_y = height
-
-
-def make_row_boxes():
+def make_row_boxes(y_counts, max_x):
     inside = False
     starts = []
     stops = []
@@ -137,17 +166,17 @@ def make_row_boxes():
     box_ys = list(zip(starts, stops))
     row_boxes = [
         Box(
-            lower_right_corner=Corner(x=max_x, y=stop),
-            lower_left_corner=Corner(x=0, y=stop),
-            upper_right_corner=Corner(x=max_x, y=start),
-            upper_left_corner=Corner(x=0, y=start),
+            lower_right_corner=Point(x=max_x, y=stop),
+            lower_left_corner=Point(x=0, y=stop),
+            upper_right_corner=Point(x=max_x, y=start),
+            upper_left_corner=Point(x=0, y=start),
         )
         for (start, stop) in box_ys
     ]
     return row_boxes
 
 
-def make_column_boxes():
+def make_column_boxes(x_counts, max_y):
     inside = False
     starts = []
     stops = []
@@ -165,23 +194,14 @@ def make_column_boxes():
     box_xs = list(zip(starts, stops))
     column_boxes = [
         Box(
-            lower_right_corner=Corner(y=max_y, x=stop),
-            lower_left_corner=Corner(y=0, x=stop),
-            upper_right_corner=Corner(y=max_y, x=start),
-            upper_left_corner=Corner(y=0, x=start),
+            lower_right_corner=Point(y=max_y, x=stop),
+            lower_left_corner=Point(y=0, x=stop),
+            upper_right_corner=Point(y=max_y, x=start),
+            upper_left_corner=Point(y=0, x=start),
         )
         for (start, stop) in box_xs
     ]
     return column_boxes
-
-
-row_boxes = make_row_boxes()
-column_boxes = make_column_boxes()
-
-number_of_rows = len(row_boxes)
-number_of_columns = len(column_boxes)
-
-print(f"{number_of_rows} rows, {number_of_columns} columns")
 
 
 def show_all_boxes_intersecting(filename, row_boxes, column_boxes):
@@ -190,7 +210,64 @@ def show_all_boxes_intersecting(filename, row_boxes, column_boxes):
             show_image_with_word_boxes(filename, [row_box, column_box])
 
 
-show_all_boxes_intersecting(filename, row_boxes, column_boxes)
+if __name__ == "__main__":
+    filename = "example.png"
+    image = cv2.imread(filename)
+    height, width, _ = image.shape
+    original_boxes = detect_text(filename)[1:]
+    boxes = original_boxes
+    max_x = max(box.max_x() for box in boxes)
+    x_counts = [len(boxes_in_x(boxes, x)) for x in range(max_x)]
+    max_y = max(box.max_y() for box in boxes)
+    boxes_in_ys = [boxes_in_y(boxes, y) for y in range(max_y)]
+    y_counts = [len(l) for l in boxes_in_ys]
+    max_x = width
+    max_y = height
+
+    row_boxes = make_row_boxes(y_counts, max_x)
+    column_boxes = make_column_boxes(x_counts, max_y)
+
+    number_of_rows = len(row_boxes)
+    number_of_columns = len(column_boxes)
+
+    rows = []
+    for i, row_box in enumerate(row_boxes):
+        new_row = []
+        for j, column_box in enumerate(column_boxes):
+            row_column = []
+            row_column_boxes = []
+            for k, original_box in enumerate(original_boxes):
+                """
+                if i == 0 and j == 2 and k == 6:
+                    show_image_with_word_boxes(filename, [row_box, column_box, original_box])
+                    print("original inside column")
+                    print(original_box.is_inside_box(column_box))
+                    print("original box")
+                    print(original_box)
+                    print("column box")
+                    print(column_box)
+                    print()
+                    print("original inside row")
+                    print(original_box.is_inside_box(row_box))
+                    print("original box")
+                    print(original_box)
+                    print("row box")
+                    print(row_box)
+
+                    row_box.plot("red")
+                    column_box.plot("blue")
+                    original_box.plot("black")
+                    plt.show()
+                """
+                if original_box.is_inside_box(column_box) and original_box.is_inside_box(row_box):
+                    row_column.append(original_box.text)
+                    row_column_boxes.append(original_box)
+            new_row.append(" ".join(row_column))
+        rows.append(new_row)
+
+    for row in rows:
+        print(",".join(row))
+    # show_image_with_word_boxes(filename)
 
 """
 import itertools
