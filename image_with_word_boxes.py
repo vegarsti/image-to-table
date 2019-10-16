8fd6ed13c27dc6380cd0b9b4c4054718271121aa
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2019-10-16 20:49:20 +0200

    Refactoring

--- image_with_word_boxes.py
+++ image_with_word_boxes.py
@@ -1,124 +1,35 @@
 import io
-from dataclasses import dataclass
 
 import cv2
-import matplotlib.pyplot as plt
 from google.cloud import vision
-from shapely import geometry
+
+from models import Box, Point
 
 
-@dataclass
-class Point:
-    x: int
-    y: int
-
-    def as_tuple(self):
-        return self.x, self.y
-
-
-@dataclass
-class Box:
-    lower_left_corner: Point
-    lower_right_corner: Point
-    upper_right_corner: Point
-    upper_left_corner: Point
-    text: str = None
-
-    def is_inside_box(self, outer_box):  # x \in X
-        return outer_box.as_polygon().contains(self.as_polygon()) or outer_box.as_polygon().overlaps(self.as_polygon())
-
-    def point_inside(self, x, y):
-        return (
-            self.lower_left_corner.x <= x <= self.upper_right_corner.x
-            and self.lower_left_corner.y <= y <= self.upper_right_corner.y
-        )
-
-    def x_inside(self, x):
-        return self.lower_left_corner.x <= x <= self.upper_right_corner.x
-
-    def y_inside(self, y):
-        return self.lower_left_corner.y <= y <= self.upper_right_corner.y
-
-    def min_x(self):
-        return min(
-            corner.x
-            for corner in (
-                self.lower_left_corner,
-                self.lower_right_corner,
-                self.upper_right_corner,
-                self.lower_right_corner,
-            )
-        )
-
-    def min_y(self):
-        return min(
-            corner.y
-            for corner in (
-                self.lower_left_corner,
-                self.lower_right_corner,
-                self.upper_right_corner,
-                self.lower_right_corner,
-            )
-        )
-
-    def max_x(self):
-        return max(
-            corner.x
-            for corner in (
-                self.lower_left_corner,
-                self.lower_right_corner,
-                self.upper_right_corner,
-                self.lower_right_corner,
-            )
-        )
-
-    def max_y(self):
-        return max(
-            corner.y
-            for corner in (
-                self.lower_left_corner,
-                self.lower_right_corner,
-                self.upper_right_corner,
-                self.lower_right_corner,
-            )
-        )
-
-    def as_polygon(self):
-        polygon = geometry.Polygon(
-            (p.x, p.y)
-            for p in (self.lower_left_corner, self.lower_right_corner, self.upper_right_corner, self.upper_left_corner)
-        )
-        assert polygon.is_valid
-        return polygon
-
-    def plot(self, color):
-        x, y = self.as_polygon().exterior.xy
-        plt.plot(x, y, color=color)
-
-
-def detect_text(image_path):
-    """Detects text in the file."""
+def detect_text(filename):
     client = vision.ImageAnnotatorClient()
-    with io.open(image_path, "rb") as image_file:
+    with io.open(filename, "rb") as image_file:
         content = image_file.read()
     image = vision.types.Image(content=content)
     response = client.text_detection(image=image)
     texts = response.text_annotations
+    boxes = [Box.from_vision_object(text) for text in texts]
+    bounding_box, *word_boxes = boxes
+    return word_boxes
 
-    boxes = []
-    for i, text in enumerate(texts):
-        lower_left_corner, lower_right_corner, upper_right_corner, upper_left_corner = [
-            Point(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices
-        ]
-        box = Box(
-            text=text.description,
-            lower_left_corner=lower_left_corner,
-            lower_right_corner=lower_right_corner,
-            upper_left_corner=upper_left_corner,
-            upper_right_corner=upper_right_corner,
-        )
-        boxes.append(box)
-    return boxes
+
+def from_vision_object(text):
+    lower_left_corner, lower_right_corner, upper_right_corner, upper_left_corner = [
+        Point(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices
+    ]
+    box = Box(
+        text=text.description,
+        lower_left_corner=lower_left_corner,
+        lower_right_corner=lower_right_corner,
+        upper_left_corner=upper_left_corner,
+        upper_right_corner=upper_right_corner,
+    )
+    return box
 
 
 def show_image_with_word_boxes(filename, boxes=None):
@@ -210,68 +121,32 @@ def show_all_boxes_intersecting(filename, row_boxes, column_boxes):
             show_image_with_word_boxes(filename, [row_box, column_box])
 
 
-if __name__ == "__main__":
-    filename = "example.png"
+def extract_table_from_image(filename):
     image = cv2.imread(filename)
     height, width, _ = image.shape
-    original_boxes = detect_text(filename)[1:]
+    original_boxes = detect_text(filename)
     boxes = original_boxes
-    max_x = max(box.max_x() for box in boxes)
-    x_counts = [len(boxes_in_x(boxes, x)) for x in range(max_x)]
-    max_y = max(box.max_y() for box in boxes)
-    boxes_in_ys = [boxes_in_y(boxes, y) for y in range(max_y)]
+    x_counts = [len(boxes_in_x(boxes, x)) for x in range(width)]
+    boxes_in_ys = [boxes_in_y(boxes, y) for y in range(height)]
     y_counts = [len(l) for l in boxes_in_ys]
-    max_x = width
-    max_y = height
-
-    row_boxes = make_row_boxes(y_counts, max_x)
-    column_boxes = make_column_boxes(x_counts, max_y)
-
-    number_of_rows = len(row_boxes)
-    number_of_columns = len(column_boxes)
-
-    rows = []
-    for i, row_box in enumerate(row_boxes):
+    row_boxes = make_row_boxes(y_counts, width)
+    column_boxes = make_column_boxes(x_counts, height)
+    table = []
+    for row_box in row_boxes:
         new_row = []
-        for j, column_box in enumerate(column_boxes):
+        for column_box in column_boxes:
             row_column = []
-            row_column_boxes = []
-            for k, original_box in enumerate(original_boxes):
-                """
-                if i == 0 and j == 2 and k == 6:
-                    show_image_with_word_boxes(filename, [row_box, column_box, original_box])
-                    print("original inside column")
-                    print(original_box.is_inside_box(column_box))
-                    print("original box")
-                    print(original_box)
-                    print("column box")
-                    print(column_box)
-                    print()
-                    print("original inside row")
-                    print(original_box.is_inside_box(row_box))
-                    print("original box")
-                    print(original_box)
-                    print("row box")
-                    print(row_box)
-
-                    row_box.plot("red")
-                    column_box.plot("blue")
-                    original_box.plot("black")
-                    plt.show()
-                """
+            for original_box in original_boxes:
                 if original_box.is_inside_box(column_box) and original_box.is_inside_box(row_box):
                     row_column.append(original_box.text)
-                    row_column_boxes.append(original_box)
             new_row.append(" ".join(row_column))
-        rows.append(new_row)
+        table.append(new_row)
+    return table
 
-    for row in rows:
+
+if __name__ == "__main__":
+    filename = "example.png"
+    table = extract_table_from_image(filename)
+
+    for row in table:
         print(",".join(row))
-    # show_image_with_word_boxes(filename)
-
-"""
-import itertools
-
-def intertwine_lists(list1, list2):
-    return list(itertools.chain.from_iterable(zip(list1, list2)))
-"""
---
+++ models.py
@@ -0,0 +1,107 @@
+from dataclasses import dataclass
+
+from matplotlib import pyplot as plt
+from shapely import geometry
+
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+    def as_tuple(self):
+        return self.x, self.y
+
+
+@dataclass
+class Box:
+    lower_left_corner: Point
+    lower_right_corner: Point
+    upper_right_corner: Point
+    upper_left_corner: Point
+    text: str = None
+
+    def is_inside_box(self, outer_box):  # x \in X
+        return outer_box.as_polygon().contains(self.as_polygon()) or outer_box.as_polygon().overlaps(self.as_polygon())
+
+    def point_inside(self, x, y):
+        return (
+            self.lower_left_corner.x <= x <= self.upper_right_corner.x
+            and self.lower_left_corner.y <= y <= self.upper_right_corner.y
+        )
+
+    def x_inside(self, x):
+        return self.lower_left_corner.x <= x <= self.upper_right_corner.x
+
+    def y_inside(self, y):
+        return self.lower_left_corner.y <= y <= self.upper_right_corner.y
+
+    def min_x(self):
+        return min(
+            corner.x
+            for corner in (
+                self.lower_left_corner,
+                self.lower_right_corner,
+                self.upper_right_corner,
+                self.lower_right_corner,
+            )
+        )
+
+    def min_y(self):
+        return min(
+            corner.y
+            for corner in (
+                self.lower_left_corner,
+                self.lower_right_corner,
+                self.upper_right_corner,
+                self.lower_right_corner,
+            )
+        )
+
+    def max_x(self):
+        return max(
+            corner.x
+            for corner in (
+                self.lower_left_corner,
+                self.lower_right_corner,
+                self.upper_right_corner,
+                self.lower_right_corner,
+            )
+        )
+
+    def max_y(self):
+        return max(
+            corner.y
+            for corner in (
+                self.lower_left_corner,
+                self.lower_right_corner,
+                self.upper_right_corner,
+                self.lower_right_corner,
+            )
+        )
+
+    def as_polygon(self):
+        polygon = geometry.Polygon(
+            (p.x, p.y)
+            for p in (self.lower_left_corner, self.lower_right_corner, self.upper_right_corner, self.upper_left_corner)
+        )
+        assert polygon.is_valid
+        return polygon
+
+    def plot(self, color):
+        x, y = self.as_polygon().exterior.xy
+        plt.plot(x, y, color=color)
+
+    @staticmethod
+    def from_vision_object(text):
+        lower_left_corner, lower_right_corner, upper_right_corner, upper_left_corner = [
+            Point(vertex.x, vertex.y) for vertex in text.bounding_poly.vertices
+        ]
+        box = Box(
+            text=text.description,
+            lower_left_corner=lower_left_corner,
+            lower_right_corner=lower_right_corner,
+            upper_left_corner=upper_left_corner,
+            upper_right_corner=upper_right_corner,
+        )
+        return box
