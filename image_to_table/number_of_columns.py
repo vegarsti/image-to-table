221fd568479feb58f47007f25bb1a758e9a57749
Andreas Bergem <bergem.andreas@gmail.com>
2019-11-24 19:26:10 +0100

    Some cleanup.

--- image_to_table/number_of_columns.py
+++ image_to_table/number_of_columns.py
@@ -1,27 +1,21 @@
-import base64
-
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import opencv_wrapper as cvw
 import scipy.ndimage as snd
 
 
-
 def find_columns(path):
     image = cv2.imread(path)
     gray = cvw.bgr2gray(image)
-
     otsu = cvw.threshold_otsu(gray, inverse=True)
 
     x_axis_sum = np.sum(otsu, axis=0).astype(np.float64)
-
     # Expand to 2-D for image operations
-    sum_image = np.expand_dims(x_axis_sum, axis=0)
-    sum_image = cvw.normalize(sum_image).astype(np.uint8)
+    x_axis_sum_2d = np.expand_dims(x_axis_sum, axis=0)
+    x_axis_sum_2d = cvw.normalize(x_axis_sum_2d).astype(np.uint8)
 
-    num_columns = find_number_of_columns(sum_image)
-    column_placement = find_column_placement(sum_image, num_columns)
+    num_columns = find_number_of_columns(x_axis_sum_2d)
+    column_placement = find_column_placement(x_axis_sum_2d, num_columns)
 
     return num_columns, column_placement
 
@@ -59,14 +53,17 @@ def find_column_placement(sum_image, num_columns):
     thresh_sum = cvw.threshold_binary(sum_image, 1)
     thresh_sum = thresh_sum[0]  # 1-D
     clipped, first_white, _ = clip(thresh_sum)
-
     is_zero = np.where(clipped == 0)[0]
     column_breaks = sorted(consecutive(is_zero), key=len)
 
-    while len(column_breaks) + 1 > num_columns:
-        clipped[column_breaks.pop(0)] = 255
+    merge_columns_until_limit(clipped, column_breaks, num_columns)
 
     return list(map(lambda x: first_white + x[-1], column_breaks))
+
+
+def merge_columns_until_limit(image, column_breaks, limit):
+    while len(column_breaks) + 1 > limit:
+        image[column_breaks.pop(0)] = 255
 
 
 # https://stackoverflow.com/questions/7352684/how-to-find-the-groups-of-consecutive-elements-from-an-array-in-numpy
